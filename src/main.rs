9604a9a05cd80770cfc2d821eff2886609409258
Tomas Drtina <tm.drtina@gmail.com>
2023-10-23 13:59:53 +0200

    Impl `cat-file`

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
-use std::env;
-use std::fs;
+use std::fs::{self, File};
+use std::io::{prelude::*, stdout};
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+use anyhow::{ensure, Context, Result};
+use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
 
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    CatFile {
+        #[arg(short = 'p')]
+        pretty_print: bool,
+        object: String,
+    },
+}
+
+fn main() -> Result<()> {
+    match Cli::parse().command {
+        Commands::Init => {
+            fs::create_dir(".git").context("Create root dir")?;
+            fs::create_dir(".git/objects").context("Create objects dir")?;
+            fs::create_dir(".git/refs").context("Create refs dir")?;
+            fs::write(".git/HEAD", "ref: refs/heads/master\n").context("Write HEAD")?;
+            println!("Initialized git directory")
+        }
+        Commands::CatFile {
+            pretty_print,
+            object,
+        } => {
+            ensure!(pretty_print, "Only pretty-print is supported!");
+            let (prefix, filename) = object.split_at(2);
+            let file = File::open(format!(".git/objects/{}/{}", prefix, filename))
+                .context("Opening object file")?;
+            let mut decoder = ZlibDecoder::new(file);
+            let mut buf = Vec::new();
+            decoder
+                .read_to_end(&mut buf)
+                .context("Reading object file")?;
+            stdout().lock().write_all(&buf).context("Writing result")?;
+        }
     }
+    Ok(())
 }
